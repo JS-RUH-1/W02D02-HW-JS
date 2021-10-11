b8328da8bd547d6ade6cd57aad7d990a394dbd16
TheRealBasel <alanaziibasel@gmail.com>
2021-10-12 02:28:36 +0300

    clean code

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ function factorial(n){
 }
 
 function FizzBuzz (n){
-    if ( n % 3 == 0 ){
-        return "Fizz";
+    if ( n % 5 == 0 && n % 3 == 0 ){
+        return "FizzBuzz";
     } else if ( n % 5 == 0 ){
         return "Buzz";
-    } else if ( n % 5 == 0 && n % 3 == 0){
-        return "FizzBuzz";
+    } else if ( n % 3 == 0){
+        return "Fizz";
     }else{
         return n;
     }
 }
+
+console.log ( FizzBuzz(15));
