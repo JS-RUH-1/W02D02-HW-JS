4621da4659f2ed1a7c388bad548a30de24fd4bb3
as-test <ashraf.alghamdi.ruh.js@tuwaiq.edu.sa>
2021-10-12 12:51:24 +0300

    Calculator added

--- task2.js
+++ task2.js
@@ -17,3 +17,51 @@ for(let i=1; i<=23; i++){
 
 document.getElementById("button").addEventListener("click", click_count)
 document.getElementById("convert").addEventListener("click", currency)
+
+/* calculator */
+function claen(){
+    document.getElementById("val1").value =  ""
+    document.getElementById("val2").value =  ""
+    document.getElementById("op").value =  "";
+    document.getElementById("equal").value = "";
+    document.getElementById("res").value = "";
+}
+function disp(id){
+    if(document.getElementById("equal").value == "="){
+        document.getElementById("equal").value = "";
+        document.getElementById("res").value = "";
+    }
+    let val = id.value;
+    document.getElementById("val2").value +=  val;
+}
+
+function operation(id){
+   document.getElementById("val1").value =  document.getElementById("val2").value;
+   document.getElementById("val2").value = "";
+   document.getElementById("op").value = id.value;
+}
+function calculate(){
+    let val1 = Number(document.getElementById("val1").value);
+    let val2 = Number(document.getElementById("val2").value);
+    let op = document.getElementById("op").value;
+    let res = 0;
+    switch(op){
+        case "X":
+            res = val1 * val2;
+            break;
+        case "/":
+            res = val1 / val2;
+            break;
+        case "+":
+            res = val1 + val2;
+            break;
+        case "-":
+            res = val1 - val2;
+            break;
+    }
+    document.getElementById("val1").value = "";
+    document.getElementById("op").value = "";
+    document.getElementById("val2").value = "";
+    document.getElementById("equal").value = "="
+    document.getElementById("res").value = res;
+}
