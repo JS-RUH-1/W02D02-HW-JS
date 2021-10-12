ee8b93e72813dcd95a18de8b6e72204ad2996c97
0Yasser <yasiirh@gmail.com>
2021-10-12 09:18:03 +0300

    second commit

--- task2.js
+++ task2.js
@@ -1,15 +1,26 @@
 
-
 let event_button=document.getElementById('button');
 document.getElementById("button").addEventListener('click',function(){
-    event_button.innerText=++counter
+    event_button.innerText=++counter;
 })
 
-let global = document.getElementById("convert").addEventListener("click",currency_convert) 
+let global = document.getElementById("convert").addEventListener("click",currency_convert);
 
 function currency_convert(){
     let rands = document.getElementById("rands").value;
-    dollars.innerText = rands * 0.066
-    euros.innerHTML = rands *0.057
-    pounds.innerHTML = rands *0.049
+    dollars.innerText = rands * 0.066 + '$';
+    euros.innerHTML = rands *0.057 + '€';
+    pounds.innerHTML = rands *0.049 + '£';
 }
+
+
+let event_elements=document.getElementById('elements');
+let global2 = window.addEventListener("load",drop_down);
+function drop_down(){
+    for(let i=1; i<=25; i++){
+        let cell = document.createElement("option");
+        cell.innerHTML = i;
+        event_elements.append(cell);
+
+    }
+} 
