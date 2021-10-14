3869e307d77c4464efd486247eae07e3e3e7772c
alanoud <alanoudmohammed958@outlook.com>
2021-10-14 09:36:05 +0300

    updated

--- task2.js
+++ task2.js
@@ -1,60 +1,50 @@
-function factorial(num){
- if (num<0)
-     return -1 ;
- else if (num ==0)
-     return 1;
- else {return (num* factorial(num-1))};
+function factorial(num) {
+    if (num < 0) return -1;
+    else if (num == 0) return 1;
+    else {
+        return num * factorial(num - 1);
+    }
 }
 factorial(5);
 
-
-function fizzbuzz(n){
- 
-    if(n % 3 == 0 && n % 5 == 0)
- return "FizzBuzz";
- else
-    if(n % 3 == 0)
- return "fizz";
- else
- if(n % 5 == 0)
- return "buzz";
- else {return n ;}
-}
-
-fizzbuzz(5)
-
-function randomNum()
-    {
-        var listOfNumbers=[];
-        var stringOutput="";
-        for (i=0;i<5;i++) {
-            listOfNumbers.push(Math.floor(Math.random()*59)+1);
-            stringOutput += listOfNumbers[i] + "";
-        }
-        document.getElementById("button").innerHTML = stringOutput;
+function fizzbuzz(n) {
+    if (n % 3 == 0 && n % 5 == 0) return "FizzBuzz";
+    else if (n % 3 == 0) return "fizz";
+    else if (n % 5 == 0) return "buzz";
+    else {
+        return n;
     }
-    document.getElementById("rands").value
-
-    function init()
-{
-   let  gbp = document.getElementById("pounds");
-   let  usd = document.getElementById("dollars");
-   let  eur = document.getElementById("euros");}
-
-   function gbpfunc()
-{
-    usd.value = parseFloat(gbp.value) * 0.49246;
-    eur.value = parseFloat(gbp.value) * 0.69714;
 }
 
-function eurfunc()
-{
-    gbp.value = parseFloat(eur.value) * 1.43448;
-    usd.value = parseFloat(eur.value) * 0.70641;}
+fizzbuzz(5);
 
-    function usdfunc()
-{
-    gbp.value = parseFloat(usd.value) * 2.03032;
-    eur.value = parseFloat(usd.value) * 1.41544;}
 
-    init();
+let clicks = 0;
+function add(){
+    clicks++;
+    document.getElementById("clicks").innerText=clicks
+};
+
+function Conversion() {
+    var val = parseInt(document.getElementById("value").value);
+    var madeSelection = parseInt(document.getElementById("Selection").value);
+
+    switch (madeSelection) {
+        case 1:
+            var converted = val * 0.068; //rands to dollars
+            break;
+        case 2:
+            var converted = val * 0.058; //rands to euros
+            break;
+        case 3:
+            var converted = val * 0.049; //rands to pounds
+            break;
+
+        default:
+            alert("You chose wrong option"); // if user chose wrong option, send him message
+            break;
+    }
+
+    document.getElementById("conversion").value = converted;
+    return false; //prevent for submit form
+}
