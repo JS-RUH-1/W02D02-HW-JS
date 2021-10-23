1a57f2800062309d111b13fd41025282eb989f9b
Masha Aldosari <md.webs95@gmail.com>
2021-10-23 20:32:07 +0300

    task 2 add

--- task1.js
+++ task1.js
@@ -15,3 +15,33 @@ function plus1(){
 }
 
 
+function rand(){
+    let randValue = document.getElementById('rand').value;
+    let label = document.getElementById('count');
+
+      let dollars = randValue * 0.067341;
+      let eure = randValue * 0.057930 ;
+      let pounds = randValue * 0.049020;
+
+      let text = `
+      Dollars:${dollars.toFixed(2)}
+      Eure:${eure.toFixed(2)}
+      Pound${pounds.toFixed(2)}`
+      label.innerHTML= text;
+}
+
+function addToDropdown() {
+    const NUM = 25;
+    let dropdownContent = document.getElementsByClassName("dropdown-content")[0];
+    
+    for (let counter = 1; counter <=  NUM; counter++) {
+        let element = document.createElement("p");
+        element.innerHTML = `${counter} - option`;
+        dropdownContent.appendChild(element);
+        console.log(element);
+    }
+}
+
+document.addEventListener("click", function(event) {
+    addToDropdown()
+})
